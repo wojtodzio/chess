98eb8638c391390526f44d3500d4bbbb6b46986d
wojtodzio <w.wrona@selleo.com>
2015-07-16 14:46:48 +0200

    Add King class and specs

---
+++ lib/king.rb
@@ -0,0 +1,5 @@
+require 'figure'
+
+class King < Figure
+  VECTORS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+end
---
+++ spec/lib/king_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'king'
+require 'support'
+
+describe King do
+  let(:example_input) { 'd4' }
+  let(:example_output) { 'c3 c4 c5 d3 d5 e3 e4 e5' }
+  let(:king) { described_class.new(example_input) }
+  it { expect(king.moves).to eq(example_output) }
+end
