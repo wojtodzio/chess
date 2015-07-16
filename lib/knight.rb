f987cdab86df582b40797333b3856f3a905bb4c4
wojtodzio <w.wrona@selleo.com>
2015-07-16 10:15:42 +0200

    Add plan

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,10 +1,17 @@
 class Knight
+  attr_reader :position
 
   def initialize(position)
-
+    @position = position
   end
 
   def moves
+    #should return all possible positions sorted
+    #convert coordinates to position
+    #compute all possible positions(add vectors to position)
+    # *define all transformation vectors
+    # *generate coordinates from position
 
   end
+
 end
