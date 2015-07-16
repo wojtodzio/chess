1cde8733d922c1a90c6d537cebb37de4c9c224e3
wojtodzio <w.wrona@selleo.com>
2015-07-16 14:11:23 +0200

    Add abstract class Figure Add shared methods of knight and pawn to figure

---
+++ lib/figure.rb
@@ -0,0 +1,36 @@
+require_relative 'support.rb'
+
+class Figure
+  include Support
+  attr_reader :position
+
+  def initialize(position)
+    @position = position
+  end
+
+  def moves
+    possible_position.join(' ')
+  end
+
+  private
+
+  def coordinate
+    @coordinate ||= Support.position_to_coordinate(position)
+  end
+
+  def possible_coordinate
+    all_coordinates.select { |coordinate| Support.is_in_board?(coordinate) }
+  end
+
+  def all_coordinates
+    self.class::VECTORS.map do |vector|
+      [coordinate.first + vector.first, coordinate.last + vector.last]
+    end
+  end
+
+  def possible_position
+    possible_coordinate.map do |coordinate|
+      Support.coordinate_to_position(coordinate)
+    end
+  end
+end
--- lib/knight.rb
+++ lib/knight.rb
@@ -1,38 +1,5 @@
-require_relative 'support.rb'
+require 'figure'
 
-class Knight
-  include Support
-  attr_reader :position
-
+class Knight < Figure
   VECTORS = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
-
-  def initialize(position)
-    @position = position
-  end
-
-  def moves
-    possible_position.join(' ')
-  end
-
-  private
-
-  def coordinate
-    @coordinate ||= Support.position_to_coordinate(position)
-  end
-
-  def possible_coordinate
-    all_coordinates.select { |coordinate| Support.is_in_board?(coordinate) }
-  end
-
-  def all_coordinates
-    VECTORS.map do |vector|
-      [coordinate.first + vector.first, coordinate.last + vector.last]
-    end
-  end
-
-  def possible_position
-    possible_coordinate.map do |coordinate|
-      Support.coordinate_to_position(coordinate)
-    end
-  end
 end
