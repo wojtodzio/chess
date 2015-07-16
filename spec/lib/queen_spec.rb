e12bbd5504c2d8a4025799138cd3bdef8221e765
wojtodzio <w.wrona@selleo.com>
2015-07-16 16:38:38 +0200

    Add Queen class with specs

---
+++ spec/lib/queen_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'queen'
+require 'support'
+
+describe Queen do
+  let(:example_input) { 'a1' }
+  let(:example_output) { 'a2 a3 a4 a5 a6 a7 a8 b1 b2 c1 c3 d1 d4 e1 e5 f1 f6 g1 g7 h1 h8' }
+  let(:queen) { described_class.new(example_input) }
+  it { expect(queen.moves).to eq(example_output) }
+end
