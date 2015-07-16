c3d378da231b3436e85aaea182bd818a05619b4f
wojtodzio <w.wrona@selleo.com>
2015-07-16 10:19:22 +0200

    Add focus to rspec

--- spec/lib/knight_spec.rb
+++ spec/lib/knight_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'knight'
 
-describe Knight do
+describe Knight, :focus do
   let(:example_input) { 'g2' }
   let(:example_output) { 'e1 e3 f4 h4' }
   let(:knight) { described_class.new(example_input) }
